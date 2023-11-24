724691e6c290175ae41d670410ff8b2f206f94df
Nelio Frazao <neliofrazac@gmail.com>
2023-11-23 23:26:22 -0300

    adiciona exemplos para aula

---
+++ desafios/accordion/js/scripts.js
@@ -0,0 +1,12 @@
+// Etapa 0
+// pega todos os elementos do header já que vamos clicar neles
+// Etapa 1
+// Adiciona evento de click em todos os headers e bota o console.log
+// Etapa 2
+// Pegga o parent node no console.log
+// Etapa 3
+// Pega todos os '.accordion-item'
+// Etapa 4
+// verifica se esta ativo para adicionar e remover as classes
+// Etapa 5
+// para cada item remve e adiciona classe de ativo e fechado
