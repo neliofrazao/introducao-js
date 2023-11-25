1432cd20783dfef651c836fdb19bce218150bb0c
Nelio Frazao <neliofrazac@gmail.com>
2023-11-25 16:56:26 -0300

    feat: desafio accordion

--- desafios/accordion/js/scripts.js
+++ desafios/accordion/js/scripts.js
@@ -1,11 +1,35 @@
 // Etapa 0
-// pega todos os elementos do header já que vamos clicar neles
+// pega todos os elementos do accordion-header já que vamos clicar neles
+const header = document.querySelectorAll(".accordion-header");
+const accordionItems = document.querySelectorAll('.accordion-item')
 // Etapa 1
 // Adiciona evento de click em todos os headers e bota o console.log
-// Etapa 2
-// Pegga o parent node no console.log
-// Etapa 3
-// Pega todos os '.accordion-item'
+
+// console.log(header)
+
+header.forEach((header) => {
+  console.log(header)
+  header.addEventListener('click', () => {
+    const item = header.parentNode
+    const isActive = item.classList.contains('accordion-item--active')
+    console.log(isActive)
+
+    console.log(!isActive)
+    // console.log('item', item)
+    // console.log('isActive', isActive)
+
+    accordionItems.forEach((item) => {
+      item.classList.remove('accordion-item--active')
+      item.classList.add('accordion-item--closed');
+    })
+
+    if(!isActive) {
+      item.classList.add('accordion-item--active');
+      item.classList.remove('accordion-item--closed')
+    }
+  })
+});
+
 // Etapa 4
 // verifica se esta ativo para adicionar e remover as classes
 // Etapa 5
